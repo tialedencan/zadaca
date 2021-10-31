3f6616a6d437f14a471a4780c461ce604acd7f21
tialedencan <tialedencan@gmail.com>
2021-10-31 09:47:43 +0100

    Added Weather.cs

---
+++ DZ1/Homework1/Weather_dz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weather_dz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DZ1/Homework1/Weather_dz/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather_dz
+{
+    class Weather
+    {
+        private double temperature;
+        private double humidity;
+        private double windSpeed;
+
+
+    }
+}
