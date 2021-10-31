211a7775f231c1030a8189e5f61ee49274cc15d1
tialedencan <tialedencan@gmail.com>
2021-10-31 13:38:11 +0100

    Constructors

--- DZ1/Homework1/Weather_dz/Weather.cs
+++ DZ1/Homework1/Weather_dz/Weather.cs
@@ -10,6 +10,20 @@ namespace Weather_dz
         private double humidity;
         private double windSpeed;
 
+
+        public Weather()
+        {
+            this.temperature = 24.3;
+            this.humidity = 1.2;
+            this.windSpeed = 12;
+        }
+        public Weather(double temperature, double humidity, double windSpeed)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.windSpeed = windSpeed;
+        }
+
         public void SetTemperature(double temperature)
         {
             this.temperature = temperature;
@@ -38,6 +52,22 @@ namespace Weather_dz
         public double GetWindSpeed()
         {
             return windSpeed;
+        }
+
+        public double CalculateFeelsLikeTemperature()
+        {
+            double heatIndex;
+            heatIndex = Constants.c1 + Constants.c2 * this.temperature + Constants.c3 * this.humidity + Constants.c4 * this.temperature * this.humidity + Constants.c5 * Math.Pow(this.temperature, 2) + Constants.c6 * Math.Pow(this.humidity, 2) + Constants.c7 * Math.Pow(this.temperature, 2) * this.humidity + Constants.c8 * this.temperature * Math.Pow(this.humidity, 2) + Constants.c9 * Math.Pow(this.temperature, 2) * Math.Pow(this.humidity, 2);
+            return heatIndex;
+        }
+
+       public double CalculateWindChill()
+        {
+            double WindChillIndex;
+            WindChillIndex = 13.12 + 0.6215*temperature - 11.37*Math.Pow(windSpeed,0.16)  + 0.3965*temperature* Math.Pow(windSpeed, 0.16);
+            
+            if(  WindChillIndex < temperature) return WindChillIndex;
+            return 0;
         }
 
 
