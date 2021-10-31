f37353bb95e958f596c0661f1cc6af70a3beaf0e
tialedencan <tialedencan@gmail.com>
2021-10-31 10:16:10 +0100

    Setters and Gettters

--- DZ1/Homework1/Weather_dz/Weather.cs
+++ DZ1/Homework1/Weather_dz/Weather.cs
@@ -10,6 +10,36 @@ namespace Weather_dz
         private double humidity;
         private double windSpeed;
 
+        public void SetTemperature(double temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public void SetWindSpeed(double windSpeed)
+        {
+            this.windSpeed = windSpeed;
+        }
+
+        public void SetHumidity(double humidity)
+        {
+            this.humidity = humidity;
+        }
+
+        public double GetTemperature()
+        {
+            return temperature;
+        }
+
+        public double GetHumidity()
+        {
+            return humidity;
+        }
+
+        public double GetWindSpeed()
+        {
+            return windSpeed;
+        }
+
 
     }
 }
