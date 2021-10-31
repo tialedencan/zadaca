80c0e037deb057ff1b559c2a9b69a8c628ad91f4
tialedencan <tialedencan@gmail.com>
2021-10-31 13:54:58 +0100

    Constants for method CalculateFeelsLikeTemperature

---
+++ DZ1/Homework1/Weather_dz/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Weather_dz
+{
+    class Constants
+    {
+        public const double c1 = -8.78469475556;
+        public const double c2 = 1.61139411;
+        public const double c3 = 2.33854883889;
+        public const double c4 = -0.14611605;
+        public const double c5 = -0.012308094;
+        public const double c6 = -0.0164248277778;
+        public const double c7 = 0.002211732;
+        public const double c8 = 0.00072546;
+        public const double c9 = -0.000003582;
+    }
+}
